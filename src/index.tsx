b336c93ef10dc9c68140273b2cb0a552a71e3320
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-05-26 15:54:11 -0400

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+// Axios for API calls
+import axios from "axios";
+axios.defaults.baseURL =
+  process.env.REACT_APP_API ||
+  "https://thumbnail-generator-backend.herokuapp.com/api/";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Auth0Provider
+          domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
+          clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
+          redirectUri={"http://localhost:3000/upload-image"}
+        >
+          <App />
+        </Auth0Provider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
