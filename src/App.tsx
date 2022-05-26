50ca1f6154876d6817a2db4127ea5482afba73eb
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-05-25 22:12:00 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import AnimatedRoutes from "./components/AnimatedRoutes/AnimatedRoutes";
+import LogoutButton from "./components/LogoutButton/LogoutButton";
 
 function App() {
+  const { isAuthenticated, user } = useAuth0();
+  isAuthenticated && console.log(user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnimatedRoutes />
+      {isAuthenticated && <LogoutButton />}
     </div>
   );
 }
