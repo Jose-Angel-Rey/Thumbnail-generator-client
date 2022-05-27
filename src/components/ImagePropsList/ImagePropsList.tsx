ff7e349b8f3c5f2bd3328a015d6f0cf3353e0a65
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-05-27 09:52:33 -0400

    Create ImagePropsList

---
+++ src/components/ImagePropsList/ImagePropsList.tsx
@@ -0,0 +1,38 @@
+import styles from "./ImagePropsList.module.scss";
+
+type ImagePropsListProps = {
+  format: string;
+  bytes: string | number;
+  height: string | number;
+  width: string | number;
+};
+
+const ImagePropsList = ({
+  format,
+  bytes,
+  width,
+  height,
+}: ImagePropsListProps) => {
+  return (
+    <ul className={styles.imagePropsList}>
+      <li className={styles.imagePropsListItem}>
+        <strong>Format: </strong>
+        <span>{format}</span>
+      </li>
+      <li className={styles.imagePropsListItem}>
+        <strong>Size: </strong>
+        <span>{bytes} bytes</span>
+      </li>
+      <li className={styles.imagePropsListItem}>
+        <strong>Height: </strong>
+        <span>{height}px</span>
+      </li>
+      <li className={styles.imagePropsListItem}>
+        <strong>Width: </strong>
+        <span>{width}px</span>
+      </li>
+    </ul>
+  );
+};
+
+export default ImagePropsList;
