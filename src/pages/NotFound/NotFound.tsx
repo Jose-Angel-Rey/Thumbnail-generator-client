f86b3dde20c67b40dd719fb8e5a1c54188bef8eb
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-05-27 06:48:26 -0400

    Create NotFound page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,9 @@
+import AnimatedPage from "../../components/AnimatedPage/AnimatedPage";
+
+export default function NotFound() {
+  return (
+    <AnimatedPage>
+      <div>NotFoundasx</div>;
+    </AnimatedPage>
+  );
+}
